18fefcb307b3f9f32e1d016a727a0fed96788c97
Tony Deng <tony@runloop.ai>
2025-10-01 17:14:12 -0700

    comment to explain deprecation

--- src/runloop_api_client/resources/devboxes/devboxes.py
+++ src/runloop_api_client/resources/devboxes/devboxes.py
@@ -897,6 +897,7 @@ class DevboxesResource(SyncAPIResource):
         )
 
     @typing_extensions.deprecated("deprecated")
+    # Use execute, executeAsync, or executeAndAwaitCompletion instead
     def execute_sync(
         self,
         id: str,
@@ -914,6 +915,9 @@ class DevboxesResource(SyncAPIResource):
         """
         Execute a bash command in the Devbox shell, await the command completion and
         return the output.
+
+        .. deprecated::
+           Use execute, executeAsync, or executeAndAwaitCompletion instead.
 
         Args:
           command: The command to execute via the Devbox shell. By default, commands are run from
@@ -2341,6 +2345,7 @@ class AsyncDevboxesResource(AsyncAPIResource):
         )
 
     @typing_extensions.deprecated("deprecated")
+    # Use execute, executeAsync, or executeAndAwaitCompletion instead
     async def execute_sync(
         self,
         id: str,
@@ -2358,6 +2363,9 @@ class AsyncDevboxesResource(AsyncAPIResource):
         """
         Execute a bash command in the Devbox shell, await the command completion and
         return the output.
+
+        .. deprecated::
+           Use execute, executeAsync, or executeAndAwaitCompletion instead.
 
         Args:
           command: The command to execute via the Devbox shell. By default, commands are run from
