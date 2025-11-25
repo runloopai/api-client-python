2f6d162f83e5dd07d377d38dc2148d633596789d
Siddarth Chalasani <siddarth@runloop.ai>
2025-11-24 17:12:34 -0800

    added helper comment (maintains consistency)

--- src/runloop_api_client/resources/scenarios/scenarios.py
+++ src/runloop_api_client/resources/scenarios/scenarios.py
@@ -918,6 +918,8 @@ class AsyncScenariosResource(AsyncAPIResource):
         run_name: Optional[str] | Omit = omit,
         run_profile: Optional[scenario_start_run_params.RunProfile] | Omit = omit,
         polling_config: PollingConfig | None = None,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
         extra_headers: Headers | None = None,
         extra_query: Query | None = None,
         extra_body: Body | None = None,
